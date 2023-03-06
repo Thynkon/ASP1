d28ec7fd31b81d5ab9de39f4db2843be2c3ea144
thynkon <thynkon@protonmail.com>
2023-03-06 14:17:41 +0100

    Add navbar and improve profile appearance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # secure by default
+  before_action :authenticate_person!, unless: :devise_controller?
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,19 +10,6 @@
 # Many of these configuration options can be set straight in your model.
 
 #! Create custom failure for turbo
-class TurboFailureApp < Devise::FailureApp
-  def respond
-    if request_format == :turbo_stream
-      redirect
-    else
-      super
-    end
-  end
-
-  def skip_format?
-    %w(html turbo_stream */*).include? request_format.to_s
-  end
-end
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -35,7 +22,6 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
   #! Change parent controller with custom controller
-  config.parent_controller = 'Users::DeviseController'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -282,7 +268,6 @@ Devise.setup do |config|
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html, :turbo_stream]
   # #! Override navigational_formats
-  config.navigational_formats = ['*/*', :html, :turbo_stream]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -302,7 +287,6 @@ Devise.setup do |config|
   # end
   #! Set custom failure
   config.warden do |manager|
-    manager.failure_app = TurboFailureApp
   #   manager.intercept_401 = false
   #   manager.default_strategies(scope: :user).unshift :some_external_strategy
   end
